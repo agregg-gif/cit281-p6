d3b00b9288223f022033c789fe4dfcc2196fd835
agregg-gif <84542325+agregg-gif@users.noreply.github.com>
2021-06-04 16:10:45 -0700

    Add files via upload

---
+++ p6.js
@@ -0,0 +1,45 @@
+class Shape {
+    constructor() {
+        this.sides = sides;
+    }   
+}
+
+const perimeter = [sides] (
+    (perimeter) => Math.add(sides.length)
+);
+
+class Rectangle {
+    constructor(length, width){
+        this.length = 0;
+        this.width = 0;
+        super(Shape) ["length", "width", "length", "width"];
+    }
+}
+//console.log(new Rectangle(4, 4).perimeter());  // 16
+//console.log(new Rectangle(4, 4).area());  // 16
+//console.log(new Rectangle(5, 5).perimeter()); // 20
+//console.log(new Rectangle(5, 5).area()); // 25
+//console.log(new Rectangle().perimeter()); // 0
+//console.log(new Rectangle().area()); // 0
+
+class Triangle {
+    constructor(sideA, sideB, sideC) {
+        sideA = 0;
+        sideB = 0;
+        sideC = 0;
+        super(shape) ["sideA", "sideB", "sideC"];
+    }
+}
+
+//console.log(new Triangle(3, 4, 5).perimeter());  // 12
+//console.log(new Triangle(3, 4, 5).area());  // 6
+//console.log(new Triangle().perimeter()); // 0
+//console.log(new Triangle().area()); // 0
+
+const data = [ [3, 4], [5, 5], [3, 4, 5], [10], [] ];
+for (const element of data) {
+    console.log(`rectangle with sides ${data} has perimeter of ${perimeter} and area of 12`);
+    console.log(`Square with sides ${data} has perimeter of ${perimeter} and area of 25`);
+    console.log(`Triangle with sides ${data} has perimeter of ${perimeter} and area of 6`);
+
+}
